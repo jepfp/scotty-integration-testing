0318f2081540f4f3ff06efa584a072112f8fee85
Philipp Jenni <philippjenni@bluemail.ch>
2016-01-12 22:21:49 +0100

    Going to docker

--- scotty-integration-testing-api/src/main/java/ch/adoray/scotty/integrationtest/installationverification/ConfigurationTest.java
+++ scotty-integration-testing-api/src/main/java/ch/adoray/scotty/integrationtest/installationverification/ConfigurationTest.java
@@ -1,12 +1,10 @@
 package ch.adoray.scotty.integrationtest.installationverification;
 
 import static ch.adoray.scotty.integrationtest.base.Configuration.config;
-import static org.junit.Assert.assertTrue;
 
-import org.json.JSONObject;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.skyscreamer.jsonassert.JSONAssert;
-import org.skyscreamer.jsonassert.JSONParser;
 
 import ch.adoray.scotty.integrationtest.common.Interactor;
 import ch.adoray.scotty.integrationtest.common.Interactor.InteractorConfigurationWithParams;
@@ -15,6 +13,7 @@ import ch.adoray.scotty.integrationtest.common.ResourceLoader;
 import com.gargoylesoftware.htmlunit.Page;
 public class ConfigurationTest {
     @Test
+    @Ignore("Subdomain not configured yet in proxy")
     public void verifySubdomain() throws Exception {
         InteractorConfigurationWithParams config = new InteractorConfigurationWithParams(config().getBaseUrl() + "/integration-testing/base/subdomain/determineSubdomain.php")//
             .disableFailOnJsonSuccessFalse();
@@ -22,13 +21,5 @@ public class ConfigurationTest {
         JSONAssert.assertEquals(ResourceLoader.loadTestData(), result.getWebResponse().getContentAsString(), false);
     }
 
-    @Test
-    public void verifyProjectPath() throws Exception {
-        InteractorConfigurationWithParams config = new InteractorConfigurationWithParams(config().getBaseUrl() + "/integration-testing/base/determineProjectPath.php")//
-            .disableFailOnJsonSuccessFalse();
-        Page result = Interactor.performRequest(config);
-        JSONObject json = (JSONObject) JSONParser.parseJSON(result.getWebResponse().getContentAsString());
-        String projectPath = (String) json.get("projectPath");
-        assertTrue("Should contain data/projects/integration-testing", projectPath.matches(".*data.projects.integration-testing.*"));
-    }
+  
 }
